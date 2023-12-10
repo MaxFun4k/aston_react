5379bb5ec9aa93dd04fb9f33ab3705876c6ab9f9
Max <maks-uznecov11@mail.ru>
2023-12-11 01:06:11 +0300

    created ErrorBounary and ErrorMessage

--- src/components/app/App.tsx
+++ src/components/app/App.tsx
@@ -1,14 +1,26 @@
 // import Routes from '../../routes/Routes.tsx';
 
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+
 import { Header } from "../header/Header";
 import { ComponentsTracks } from "../componentsTracks/ComponentsTracks";
+import ErrorBounary from "../errorBounary/ErrorBounary";
 
 export function App() {
 	return (
-		<>
+		// <>
+		// 	<Header/>
+		// 	<ErrorBounary>
+		// 		<ComponentsTracks/>
+		// 	</ErrorBounary>
+		// </>
+		<Router>
 			<Header/>
-			<ComponentsTracks/>
-		</>
-		// <Routes/>
+			<main>
+				<Routes>
+					<Route path="/" element = {<ComponentsTracks/>}/>
+				</Routes>
+			</main>
+		</Router>
 	);
 }
---
+++ src/components/errorMessage/ErrorMessage.tsx
@@ -0,0 +1,10 @@
+import {FC} from "react";
+import "./ErrorMessage.css";
+
+const ErrorMessage:FC = () => {
+	return (
+		<div className="error">Упс... ошибка!</div>
+	);
+};
+
+export default ErrorMessage;
